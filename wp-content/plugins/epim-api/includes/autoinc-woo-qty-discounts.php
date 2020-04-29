ce6cdf37b619d887bb2588aee0be52ae78b5764e
EdwardN1 <edward@technicks.com>
2020-04-29 10:30:14 +0100

    QTY Discounts

--- wp-content/plugins/epim-api/includes/autoinc-woo-qty-discounts.php
+++ wp-content/plugins/epim-api/includes/autoinc-woo-qty-discounts.php
@@ -15,15 +15,50 @@ function epim_quantity_based_pricing( $cart ) {
 
     foreach ( $cart->get_cart() as $cart_item_key => $cart_item ) {
         $id = $cart_item['data']->get_id();
-        $threshold1 = get_post_meta($id,'epim_Qty_Break_1',true);
 
-        if ( $cart_item['quantity'] >= $threshold1 && $cart_item['quantity'] < $threshold2 ) {
-            $price = round( $cart_item['data']->get_price() * ( 1 - $discount1 ), 2 );
-            $cart_item['data']->set_price( $price );
-        } elseif ( $cart_item['quantity'] >= $threshold2 ) {
-            $price = round( $cart_item['data']->get_price() * ( 1 - $discount2 ), 2 );
-            $cart_item['data']->set_price( $price );
+        $epim_threshold1 = isInteger(get_post_meta($id,'epim_Qty_Break_1',true));
+        $epim_threshold2 = isInteger(get_post_meta($id,'epim_Qty_Break_2',true));
+        $epim_threshold3 = isInteger(get_post_meta($id,'epim_Qty_Break_3',true));
+
+        $epim_price1 = get_post_meta($id,'epim_Qty_Price_1',true);
+        $epim_price2 = get_post_meta($id,'epim_Qty_Price_2',true);
+        $epim_price3 = get_post_meta($id,'epim_Qty_Price_3',true);
+
+        $price = $cart_item['data']->get_price();
+
+        if(($epim_price1 != '')||($epim_price2 != '')||($epim_price3 != '')) {
+            if(($epim_threshold1)&&($epim_price1 != '')) {
+                if($cart_item['quantity'] >= $epim_threshold1) {
+                    $price = $epim_price1;
+                }
+            }
+            if(($epim_threshold2)&&($epim_price2 != '')) {
+                if($cart_item['quantity'] >= $epim_threshold2) {
+                    $price = $epim_price2;
+                }
+            }
+            if(($epim_threshold3)&&($epim_price3 != '')) {
+                if($cart_item['quantity'] >= $epim_threshold3) {
+                    $price = $epim_price3;
+                }
+            }
         }
+
+        $cart_item['data']->set_price( $price );
+
     }
 
+}
+
+function isInteger($value) {
+    if(!$value) {
+        return false;
+    }
+    if($value=='') {
+        return false;
+    }
+    if(!ctype_digit($value)) {
+        return false;
+    }
+    return (int)$value;
 }
