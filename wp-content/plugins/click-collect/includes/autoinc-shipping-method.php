106103ee4ac6b56e2721f06056d38a2b69a42b8a
EdwardN1 <edward@technicks.com>
2020-05-14 08:46:44 +0100

    Stock Level at Branch Verification

--- wp-content/plugins/click-collect/includes/autoinc-shipping-method.php
+++ wp-content/plugins/click-collect/includes/autoinc-shipping-method.php
@@ -5,6 +5,11 @@ if (!defined('WPINC')) {
 }
 
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+
+    /**
+     * Create the Click and Collect Shipping Method
+     */
+
     function click_collect_method()
     {
         if (!class_exists('click_collect_method')) {
@@ -127,15 +132,19 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             }
         }
     }
-
     add_action('woocommerce_shipping_init', 'click_collect_method');
 
+    /**
+     * @param $methods
+     * @return mixed
+     *
+     * Add the Click and Collect Shipping Method
+     */
     function add_click_collect_method($methods)
     {
         $methods[] = 'click_collect_method';
         return $methods;
     }
-
     add_filter('woocommerce_shipping_methods', 'add_click_collect_method');
 
     //This function is unused: cac_store_row_layout()
@@ -186,6 +195,11 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
         }
     }
 
+    /**
+     * @param $posted
+     *
+     * Validates the Checkout Page
+     */
     function cac_validate_order($posted)
     {
         $packages = WC()->shipping()->get_packages();
@@ -238,9 +252,114 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             }
         }
     }
-
     add_action('woocommerce_review_order_before_cart_contents', 'cac_validate_order', 10);
     add_action('woocommerce_after_checkout_validation', 'cac_validate_order', 10);
+
+    /**
+     * @param $button
+     * @return string
+     *
+     * Disables the checkout button if Click and Collect not validated
+     */
+    function inactive_order_button_html( $button ) {
+        $found = false;
+
+        $packages = WC()->shipping()->get_packages();
+
+        $chosen_methods = WC()->session->get('chosen_shipping_methods');
+
+        if (is_array($chosen_methods) && in_array('clickcollect', $chosen_methods)) {
+            foreach ($packages as $i => $package) {
+                if ($chosen_methods[$i] != 'clickcollect') {
+                    continue;
+                }
+            }
+
+            $frmData = array();
+            parse_str(urldecode($_POST['post_data']), $frmData);
+            $shipping_COLLECTION_BRANCH = $frmData['shipping_COLLECTION_BRANCH'];
+
+            if($shipping_COLLECTION_BRANCH) {
+
+                foreach ($package['contents'] as $item_id => $values) {
+                    $_product = $values['data'];
+                    $cac_USE_BRANCH_STOCK = get_post_meta($_product->get_id(), 'cac_USE_BRANCH_STOCK');
+                    if ($cac_USE_BRANCH_STOCK && ($cac_USE_BRANCH_STOCK[0] === 'yes')) {
+
+                        $branchStockLevelMETA = get_post_meta($_product->get_id(), 'cac_BRANCH_STOCK_' . $shipping_COLLECTION_BRANCH);
+                        $branchStockLevel = $branchStockLevelMETA[0];
+
+                        //error_log('$branchStockLevel = ' . print_r($branchStockLevel, true) . ' For BranchID = ' . $shipping_COLLECTION_BRANCH);
+
+                        if (!($branchStockLevel) || ($branchStockLevel < 1)) {
+
+                            $found = true;
+                        }
+                    } else {
+                        //error_log('not branch stock selected');
+                    }
+                }
+            } else {
+                //error_log('no branch found');
+            }
+        }
+
+        // If found we replace the button by an inactive greyed one
+        if( $found ) {
+            $style = 'style="background:Silver !important; color:white !important; cursor: not-allowed !important; float: right;"';
+            $button_text = apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) );
+            $button = '<a class="button" '.$style.'>' . $button_text . '</a>';
+        }
+        return $button;
+    }
+    add_filter('woocommerce_order_button_html', 'inactive_order_button_html' );
+
+    //Adjusts Branch Stock
+    add_action( 'woocommerce_thankyou', 'cac_update_frm_entry_after_wc_order_completed' );
+    function cac_update_frm_entry_after_wc_order_completed( $order_id ) {
+        if ( ! $order_id ) return;
+        $order = new WC_Order( $order_id );
+        $ship_array =$order->get_items( 'shipping' );
+        $shipping = reset( $ship_array )->get_method_id();
+        if($shipping==='clickcollect') {
+            $collectionBranch = get_post_meta($order_id,'_COLLECTION_BRANCH_NAME',true);
+            error_log('_COLLECTION_BRANCH = '.print_r($collectionBranch,true));
+            foreach ( $order->get_items() as $item_id => $item ) {
+
+                if( $item['variation_id'] > 0 ){
+                    $product_id = $item['variation_id']; // variable product
+                } else {
+                    $product_id = $item['product_id']; // simple product
+                }
+
+                // Get the product object
+                $product = wc_get_product( $product_id );
+
+                $args = array(
+                    'post_type' => 'branches',
+                    'post_status' => 'publish',
+                    'posts_per_page' => -1,
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                );
+
+                $branches = new WP_Query($args);
+                while ($branches->have_posts()) : $branches->the_post();
+                    $id = get_the_ID();
+                    $branchName = get_the_title();
+
+
+                endwhile;
+
+                wp_reset_postdata();
+
+            }
+        }
+
+
+    }
+
+
 
 }
 
