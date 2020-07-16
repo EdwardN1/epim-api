002b8a1d7eea14f289574565e69ac31215d44f90
EdwardN1 <GH?841241ed>
2020-07-16 19:05:58 +0100

    WP Account Manager

--- wp-content/plugins/wp-account-manager/wp-account-manager.php
+++ wp-content/plugins/wp-account-manager/wp-account-manager.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: WP Account Manager
 Plugin URI: https://www.technicks.com
-Description: Implements a separate account management system for Worpdress to protect front end pages without access to WP users or CMS.
+Description: Implements a separate account management system for Wordpress to protect front end pages without access to WP admin panel.
 Author: Edward Nickerson
 Version: 1.0.0
 Author URI: https://www.technicks.com
@@ -16,5 +16,5 @@ define('wpam_FUNCTIONSPATH', plugin_dir_path( __FILE__ ) . '/includes/');
 define('wpam_PLUGINPATH', plugin_dir_path( __FILE__ ) );
 foreach (glob(wpam_FUNCTIONSPATH.'autoinc-*.php') as $filename)
 {
-    require_once ($filename);
+	require_once ($filename);
 }
