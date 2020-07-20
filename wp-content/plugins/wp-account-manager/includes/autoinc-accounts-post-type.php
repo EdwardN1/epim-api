a685260eeef666173b15186df9cb6d5e893128ab
EdwardN1 <GH?841241ed>
2020-07-20 17:18:07 +0100

    WP Account Manager

--- wp-content/plugins/wp-account-manager/includes/autoinc-accounts-post-type.php
+++ wp-content/plugins/wp-account-manager/includes/autoinc-accounts-post-type.php
@@ -291,6 +291,173 @@ function save_wpam_accounts_email_meta_box_data( $post_id ) {
 
 add_action( 'save_post', 'save_wpam_accounts_email_meta_box_data' );
 
+/**
+ *  =========================== Account account_enabled ==================================
+ */
+
+function wpam_accounts_account_enabled_meta_box() {
+	add_meta_box(
+		'wpam_accounts_account_enabled_container',
+		__( 'Account Enabled', 'wpamaccounts' ),
+		'wpam_accounts_account_enabled_meta_box_callback',
+		'wpam_accounts'
+	);
+}
+
+function wpam_accounts_account_enabled_meta_box_callback( $post ) {
+
+	// Add a nonce field so we can check for it later.
+	wp_nonce_field( 'wpam_accounts_account_enabled_nonce', 'wpam_accounts_account_enabled_nonce' );
+
+	$value = get_post_meta( $post->ID, '_wpam_accounts_account_enabled', true );
+
+	echo '<label for="wpam_accounts_account_enabled"><input type="checkbox" id="wpam_accounts_account_enabled" name="wpam_accounts_account_enabled" value="yes" ';
+	if(isset($value)) checked($value.'yes');
+	echo ' /> Account Enabled</label>';
+}
+
+add_action( 'add_meta_boxes', 'wpam_accounts_account_enabled_meta_box' );
+
+function save_wpam_accounts_account_enabled_meta_box_data( $post_id ) {
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['wpam_accounts_account_enabled_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['wpam_accounts_account_enabled_nonce'], 'wpam_accounts_account_enabled_nonce' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	}
+	else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+
+	// Make sure that it is set.
+	if ( ! isset( $_POST['wpam_accounts_account_enabled'] ) ) {
+		return;
+	}
+
+	// Sanitize user input.
+	if(isset($_POST['wpam_accounts_account_enabled'])) {
+		update_post_meta( $post_id, '_wpam_accounts_account_enabled', 'yes' );
+	} else {
+		update_post_meta( $post_id, '_wpam_accounts_account_enabled', 'no' );
+	}
+
+}
+
+add_action( 'save_post', 'save_wpam_accounts_account_enabled_meta_box_data' );
+
+
+/**
+ *  =========================== Account Registered Time ==================================
+ */
+
+function wpam_accounts_registered_time_meta_box() {
+	add_meta_box(
+		'wpam_accounts_register_time_container',
+		__( 'Account Registered Time', 'wpamaccounts' ),
+		'wpam_accounts_registered_time_meta_box_callback',
+		'wpam_accounts'
+	);
+}
+
+function wpam_accounts_registered_time_meta_box_callback( $post ) {
+
+	// Add a nonce field so we can check for it later.
+	wp_nonce_field( 'wpam_accounts_registered_time_nonce', 'wpam_accounts_registered_time_nonce' );
+
+	$value = get_post_meta( $post->ID, '_wpam_accounts_registered_time', true );
+
+	echo '<p style="width:100%" id="wpam_accounts_registered_time">' . esc_attr( $value ).'</p>';
+}
+
+add_action( 'add_meta_boxes', 'wpam_accounts_registered_time_meta_box' );
+
+function save_wpam_accounts_registered_time_meta_box_data( $post_id, $post, $update ) {
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['wpam_accounts_email_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['wpam_accounts_email_nonce'], 'wpam_accounts_email_nonce' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	}
+	else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+
+	$is_new = $post->post_date === $post->post_modified;
+
+	// Make sure that it is a new post.
+	if ( !$is_new ) {
+		return;
+	}
+
+	// Sanitize user input.
+	$my_data = date(DATE_RFC2822);
+
+	error_log('Date is: '.$my_data);
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_wpam_accounts_registered_time', $my_data );
+
+
+}
+
+add_action( 'save_post', 'save_wpam_accounts_registered_time_meta_box_data', 10, 3 );
+
+
+/**
+ * @param $username
+ * @param $exclude
+ *
+ * @return bool
+ *
+ * **** Various post related functions
+ */
+
 function wpam_unique_username($username,$exclude) {
 	$args = array(
 		'title' => $username,
