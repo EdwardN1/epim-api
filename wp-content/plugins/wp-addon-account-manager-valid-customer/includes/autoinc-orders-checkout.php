7a757c90c7d98ffb9f1a32f7b11d3bea0e9af7c0
EdwardN1 <GH?841241ed>
2020-09-09 16:02:05 +0100

    Update Directory Path for ePim API Importer

--- wp-content/plugins/wp-addon-account-manager-valid-customer/includes/autoinc-orders-checkout.php
+++ wp-content/plugins/wp-addon-account-manager-valid-customer/includes/autoinc-orders-checkout.php
@@ -26,7 +26,15 @@ function wpamvc_checkout_login_form() {
 	echo '<div class="woocommerce-info"> Returning customer? <a href="'.get_permalink( get_option('woocommerce_myaccount_page_id') ).'">Click here to login</a>	</div>';
 }
 
-//add_action( 'woocommerce_order_status_completed', 'wpamvc_woocommerce_order_status_completed', 10, 1 );
+add_action( 'woocommerce_order_status_completed', 'wpamvc_woocommerce_order_status_completed', 10, 1 );
 function wpamvc_woocommerce_order_status_completed( $order_id ) {
-	error_log( "Order complete for order".$order_id);
+	error_log( "============================== Order complete for order ============================".$order_id);
 }
+
+add_action( 'woocommerce_order_status_processing', 'wpamvc_woocommerce_order_status_processing', 10, 1 );
+function wpamvc_woocommerce_order_status_processing( $order_id ) {
+	error_log( "============================== Order processing for order ============================".$order_id);
+}
+
+
+
