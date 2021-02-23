86cd3ffca564c681cfca204d00596f8348755166
EdwardN1 <GH?841241ed>
2021-02-23 17:54:54 +0000

    Cron Jobs Setup every minute and every 20 minutes

--- wp-content/plugins/wp-infor-api/includes/autoinc-cron.php
+++ wp-content/plugins/wp-infor-api/includes/autoinc-cron.php
@@ -1,18 +1,27 @@
 <?php
-register_activation_hook(__FILE__, 'wpiai_cron_activation');
+register_activation_hook(wpiai_PLUGINFILE, 'wpiai_cron_activation');
 
 function wpiai_cron_activation() {
-    error_log('Running wpiai_cron_activation');
+    //error_log('Running wpiai_cron_activation');
     if (! wp_next_scheduled ( 'wpiai_every_minute_action' )) {
         wp_schedule_event(time(), 'everyminute', 'wpiai_every_minute_action');
     }
+	if (! wp_next_scheduled ( 'wpiai_every_twenty_minutes_action' )) {
+		wp_schedule_event(time(), 'everytwentyminutes', 'wpiai_every_twenty_minutes_action');
+	}
 }
 
 add_action('wpiai_every_minute_action', 'wpiai_do_every_minute');
+add_action('wpiai_every_twenty_minutes_action', 'wpiai_do_every_twenty_minutes');
 
 function wpiai_do_every_minute() {
     // do something every minute
-    error_log('WP Cron is working....');
+    //error_log('WP Cron is working....');
+}
+
+function wpiai_do_every_twenty_minutes() {
+	// do something every twenty minutes
+	//error_log('WP Cron is working....');
 }
 
 add_filter( 'cron_schedules', 'wpiai_add_cron_interval' );
@@ -21,5 +30,9 @@ function wpiai_add_cron_interval( $schedules ) {
         'interval'  => 60, // time in seconds
         'display'   => 'Every Minute'
     );
+	$schedules['everytwentyminutes'] = array(
+		'interval'  => 1200, // time in seconds
+		'display'   => 'Every Twenty Minutes'
+	);
     return $schedules;
 }
--- wp-content/plugins/wp-infor-api/wp-infor-api-interface.php
+++ wp-content/plugins/wp-infor-api/wp-infor-api-interface.php
@@ -17,31 +17,6 @@ define('wpiai_PLUGINPATH', plugin_dir_path( __FILE__ ) );
 define('wpiai_PLUGINURI',plugin_dir_url(__FILE__));
 define('wpiai_PLUGINFILE',__FILE__);
 
-register_activation_hook(__FILE__, 'wpiai_cron_activation');
-
-function wpiai_cron_activation() {
-    error_log('Running wpiai_cron_activation');
-    if (! wp_next_scheduled ( 'wpiai_every_minute_action' )) {
-        wp_schedule_event(time(), 'everyminute', 'wpiai_every_minute_action');
-    }
-}
-
-add_action('wpiai_every_minute_action', 'wpiai_do_every_minute');
-
-function wpiai_do_every_minute() {
-    // do something every minute
-    error_log('WP Cron is working....');
-}
-
-add_filter( 'cron_schedules', 'wpiai_add_cron_interval' );
-function wpiai_add_cron_interval( $schedules ) {
-    $schedules['everyminute'] = array(
-        'interval'  => 60, // time in seconds
-        'display'   => 'Every Minute'
-    );
-    return $schedules;
-}
-
 foreach (glob(wpiai_FUNCTIONSPATH.'autoinc-*.php') as $filename)
 {
 	require_once ($filename);
