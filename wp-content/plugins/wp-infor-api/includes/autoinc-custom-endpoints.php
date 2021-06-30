349aa7910c731eb5cf667d088a44e12fe639eb5f
EdwardN1 <GH?841241ed>
2021-06-30 16:29:57 +0100

    INFOR Sales Order Cached API

--- wp-content/plugins/wp-infor-api/includes/autoinc-custom-endpoints.php
+++ wp-content/plugins/wp-infor-api/includes/autoinc-custom-endpoints.php
@@ -1,10 +1,22 @@
 <?php
 function set_csdorders( $request ) {
 	//return array( 'csdorders' => 'Data' );
-	$d = json_decode($request->get_body());
-	$r = $d->meta_data;
-	return $r;
-
+	$body = json_decode($request->get_body());
+	$meta = $body->meta_data;
+	if(is_array($meta)) {
+		$CSD_ID = false;
+		foreach ($meta as $key_value) {
+			if($key_value->key=='CSD_ID') {
+				$CSD_ID = $key_value->value;
+			}
+		}
+		if($CSD_ID) {
+			return array( 'CSD_ID' => $CSD_ID );
+		}
+		return array( 'error' => 'could not find CSD_ID in meta_data' );
+	} else {
+		return array( 'error' => 'No meta could not find CSD_ID' );
+	}
 }
 
 add_action( 'rest_api_init', function () {
