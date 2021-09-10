bc86030fcbb0620455937c6ab5083907e42dfe58
EdwardN1 <GH?841241ed>
2021-09-10 16:40:15 +0100

    EPIM Background Processes

--- wp-content/plugins/epim-api-importer/includes/autoinc-ajax-handlers.php
+++ wp-content/plugins/epim-api-importer/includes/autoinc-ajax-handlers.php
@@ -114,7 +114,8 @@ function ajax_get_epimaapi_background_changed_products_since() {
 
 function ajax_epimaapi_fast_create() {
 	epimaapi_checkSecure();
-
+	//update_option('_epim_update_running','Categories Updated and Sorted');
+	//update_option('_epim_background_current_index',5400);
 	$epim_update_running = get_option( '_epim_update_running' );
 	if ( $epim_update_running == '' ) {
         //echo epimaapi_background_import_all_start();
