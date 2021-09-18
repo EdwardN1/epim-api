4c17147939ea022ad085d45ec136e68baabe4f7f
Edward Nickerson <edward@technicks.com>
2021-09-18 06:51:11 +0100

    ePim Background Updates

--- wp-config.php
+++ wp-config.php
@@ -13,29 +13,33 @@
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
+ * @link https://codex.wordpress.org/Editing_wp-config.php
  *
  * @package WordPress
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'c76gosparkystage');
+define('DB_NAME', "c76targetlive");
 
 /** MySQL database username */
-define('DB_USER', 'c76gosparkystage');
+define('DB_USER', "c76targetlive");
 
 /** MySQL database password */
-define('DB_PASSWORD', 'GS?841241ed');
+define('DB_PASSWORD', "TA?841241ed");
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', "127.0.0.1");
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+/** Disable WP Cron use cron job instead */
+
+define( 'DISABLE_WP_CRON', true );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +50,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         ':]rESS--7<=2Ti61o|+hv}OGTa%zB11C%)&z8J=--/60FZUIDsR.EX->,XwKaroc' );
-define( 'SECURE_AUTH_KEY',  'HWz(Hj3.@VKlk3ON2D>lGL2,Lq0m~|;20VP9gY@^x;nQaWEy`A0LtZ/7nn MN<JN' );
-define( 'LOGGED_IN_KEY',    '5^zhX1DCvay.?%r/a)buV~2c[41c1u_1qdWGGwbQu5wNdO`8O7u6X>Ai>l=}0cOo' );
-define( 'NONCE_KEY',        'VVyhqp{=v1E,+;,,<Qg=Xe[:86C4:hmbGE>-JW#xm3I>W;NdUrYc^_%|M{d N=7)' );
-define( 'AUTH_SALT',        'Y@%<T_1SX~um:aPhl1E-^]iS~8rEF`p LdRv!8^}@$%*p$|`y6rVju5v@;UzCrB<' );
-define( 'SECURE_AUTH_SALT', 'wfot|)c&+RCh[OR@,$thl/4})_Q;q$W%WfEYP`]DL;btHq;1#SW@b&DSvp9QHH3^' );
-define( 'LOGGED_IN_SALT',   '>W;aVkJMfFd^:Fu>wgoowX|@9_lpr]A8Jk09^0Ew}-&m1@<28H1F+.ndR^JSo_N-' );
-define( 'NONCE_SALT',       'AIq~0|V=5ZVlay;E!2ZhLPl<zm>gS1=XAGzC=_|/:Z[6s }NsQ9RYHocob&K_{bB' );
+define( 'AUTH_KEY',         'xc<)fDjlAWz1OQ3fqDGMmnck )vF!ha4FQo64aHT:@e?|h),iGC+>ySyf6`@d*CX' );
+define( 'SECURE_AUTH_KEY',  'OD2?7{z~7R0Td K8$,WRTl~pT(y+rweGin9Rjr>MD#/MyOO_6J4#6 J|4WvpBfVY' );
+define( 'LOGGED_IN_KEY',    'o`n8Szzjzr jdFh~:M)#N`sAjlC<StP,Nyk@AiQb&VildY&t}{M)P}}15jY#5!WK' );
+define( 'NONCE_KEY',        'JwhisJ-,}oNGekFmZ@JNoSXCicBPG94)ID@oROr;?rcS}2,ms#&&-a6Vt/P&FB(Y' );
+define( 'AUTH_SALT',        ')9N0ps1`_;rz>G[3o.n;<  !$nC[.gYmj{1lqx)i4r_L8]S^wx!#AY`C:63ybf20' );
+define( 'SECURE_AUTH_SALT', '*a!lMqOvUjO qIq}Nx#UR6|.yF]qypNWT$-[N;}^cRCgW#r_q#7W*yNGe9<6ShNT' );
+define( 'LOGGED_IN_SALT',   ',.d}[oe^$x<)}A74K:GCXYu_m.,V?I}lm&CRGpU a]1i4er(egSrIE`)ZElcD=2^' );
+define( 'NONCE_SALT',       'ua|%9{?.K;sw`8I%bN[hw[&;j_3@8&/U9;e66sGkqfHaya!{08BD?c,n<E`27Ga]' );
 
 /**#@-*/
 
@@ -73,21 +77,19 @@ $table_prefix = 'wp_';
  * in their development environments.
  *
  * For information on other constants that can be used for debugging,
- * visit the documentation.
+ * visit the Codex.
  *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
-
-define( 'WP_MEMORY_LIMIT', '2048M' );
 define('DISABLE_WP_CRON', true);
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', dirname(__FILE__) . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
+require_once( ABSPATH . 'wp-settings.php' );
