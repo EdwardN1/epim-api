a6ed1ab769fec86898b113b01e082b7f8e7b8b44
edwar <edward@technicks.com>
2022-03-22 10:09:56 +0000

    Kosnic

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', "erfelect_wp_khmde");
+define( 'DB_NAME', "c76kosnicold" );
 
 /** MySQL database username */
-define('DB_USER', "erfelect_wp_fwfml");
+define( 'DB_USER', "c76kosnicold" );
 
 /** MySQL database password */
-define('DB_PASSWORD', "a#d4L1Eey~^\$Us2v");
+define( 'DB_PASSWORD', "KO?841241ed" );
 
 /** MySQL hostname */
-define('DB_HOST', "localhost:3306");
+define( 'DB_HOST', "127.0.0.1" );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', 'utf8_unicode_ci' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,26 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-/*
-Changed by Josh 10:47 - Wed 18th August // Need to force logout all users
-define('AUTH_KEY',         'Z|T(`]gR++/|.+?0w>w7`9# e_5 (N?THoZ- r%+h*of(-;~~K;v&$4N~K.VppP-');
-define('SECURE_AUTH_KEY',  'tBS~I|m~JwqbUE[6H@(!uLDI:0|%ez]<TI!=Q.|EP-q+#kbD{&GN(4Fk8O3ST.CT');
-define('LOGGED_IN_KEY',    ':DgAmB7# :Q!>S4+1B`QW6{xUSh<!HUq<4}5Q2>u#g92+6/i53!=PLA4*!R<;@-Y');
-define('NONCE_KEY',        ']cm.dAazJrIu*kF|B{A^vBWR<+u?J1*Fg3$*khr@>OF1Ciz|%Dd<}x&6^M!PDj0^');
-define('AUTH_SALT',        'y$6 m+JTTEqQs|+Z- k}./Qp2o9$s^kB>NMEbz3QVN@AX{[_oykmJ]]l<-h-qZvd');
-define('SECURE_AUTH_SALT', 'E3R4;}4vV5*R@2|*T3e?Q9Z@6+T8RA-8r9cnj{7U[2zsNGxA+ql`j&N*k5Q N!/O');
-define('LOGGED_IN_SALT',   '?*LAs+XGS/LS2P-`o/g-R|-aXsf/CvU5e(=WL6)BK+k#R?:&A[ee,>9q}beG*L+Q');
-define('NONCE_SALT',       '4}M96;*W8z[P7S.|vD1;jgQ^l. (>@6|4aglc5C3_uMRrwmoG++xWzL1n`GxE%/>');
-*/
-define('AUTH_KEY',         '1$Q^Xm-++wAZi*`p6|FEx@-h<O+qS^Ku^+L2rN>*9t Myu{y}l}M**|7+7&g%|J/');
-define('SECURE_AUTH_KEY',  '4/+|W!NSEL|p `wk#q.zjc 3*7ZC;)]q8j!jwl7|-9d9d&!;=W_MXk@6~|wWg!>d');
-define('LOGGED_IN_KEY',    'mN<Y&&4?+VNCw!p34?-q7)amku^YQ2<(zB-f|+CmR/vk^dO;dh/z|%Vk>HRS ;vI');
-define('NONCE_KEY',        '}j>^3d7aXHd~LlANud$z!%Zg#c/xN+w_4u,(,n6%Ijt2uu({M>kH-of!bLa6S&2Z');
-define('AUTH_SALT',        'xkq|:PuLytA#_ES-L<INfM01=~=z$:[_)~q|(UqL|yOpqMB=>[pag6#d&=-f8HP ');
-define('SECURE_AUTH_SALT', '/)_rFd:;iQ.+IDxglZ@+Ft/%W*LEszCGieY+_!ZdU{/mO1#zCNV$|G:uQSKb#J=J');
-define('LOGGED_IN_SALT',   'E0d:GgMmbE6<M}%?NSyOVE+K|0.5R{,2nh/97poW-Tor9{|$STz+GSv,C|JMjO(d');
-define('NONCE_SALT',       'I>Dn[TS6#Gx`cVy[T?lt>}PnnfWU4LLe(+7fQ[){$Kv+*Zl|v*w#K>1TmyV!D !N');
-
+define( 'AUTH_KEY',         'yT?7f{U:i%>b!lz=u3wA<gofsK;pQMHLM%y{TYo9mwz6@luX95:r=YXu k)1vZI8' );
+define( 'SECURE_AUTH_KEY',  '$ %RxhP#yX}rf)d|I0tp>QLr6N:CiGr^MdMVV,mPYbPfl:.q`5B(b*W_<gr^J^::' );
+define( 'LOGGED_IN_KEY',    'I/X?>c5XH!pCv}<Mb_2x<0{TNRVpG.s0G`=}|HoG2E}C0PY@f>HIw6H9nn+`NnU?' );
+define( 'NONCE_KEY',        'azB0^rZ{UA,1[Wc28M5*8@TT[|stTcK+au|z16U[e/^C_)EQz`D9?BAF!;^bI/~H' );
+define( 'AUTH_SALT',        'I$IjJR.b<Iq,iCRAM]JjFO$1+2>M 7];WGXZd2Y|S4?6E}N2w1~+JvkkQk^Tn{D)' );
+define( 'SECURE_AUTH_SALT', 'qaYoR-,g1}5]e1)!xdDF6(AQ6;&|v/%]BeBj+CSJ+g5,R8X/tXu+|Kn|{jcdV)i/' );
+define( 'LOGGED_IN_SALT',   'S,9e> qv _V#>t,qe~:Buvg)|az0~J^&`ep,]h/7 s|yIv*a~,RndX2%;L<&o;w:' );
+define( 'NONCE_SALT',       'mAXz]?G[e#i=o_f E1Ardr(e]&n8,k?tKjt&mazbM}Sp!=@+iD 8-CtOXmK bYcc' );
 
 /**#@-*/
 
@@ -89,9 +77,13 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+define( 'DISALLOW_FILE_EDIT', false );
+define( 'DISALLOW_FILE_MODS', false );
+define( 'WP_POST_REVISIONS', 0 );
+define( 'DISABLE_WP_CRON', false );
+define( 'WP_AUTO_UPDATE_CORE', false );
+define( 'DUP_SECURE_KEY', '854c368a13b6f2a5a58fd5e0556d2698' );
 define('DISABLE_WP_CRON', true);
-
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
@@ -101,12 +93,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
-
-define( 'WP_MEMORY_LIMIT', '120M' );
-
-define('WP_FAIL2BAN_ADDON_BLOCKLIST_IGNORE_IPS', [
-	'195.224.67.34',
-    '176.251.222.95',
-    '185.75.213.31'
-]);
-
