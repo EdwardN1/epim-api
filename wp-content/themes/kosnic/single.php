46b34ff6ac68d20ac597676e36b22e77c559b8fb
EdwardN1 <GH?841241ed>
2022-03-30 14:45:12 +0100

    Kosnic

--- wp-content/themes/kosnic/single.php
+++ wp-content/themes/kosnic/single.php
@@ -22,14 +22,48 @@
                 ?>
                 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
                     <div class="grid-x">
-                        <div class="cell large-4 medium-3 small-12">
+                        <div class="cell large-4 medium-3 small-12 image-gallery">
                             <?php do_action( 'woocommerce_before_single_product_summary' );?>
                         </div>
-                        <div class="cell large-8 medium-9 small-12">
-                            <?php do_action( 'woocommerce_after_single_product_summary' );?>
+                        <div class="cell large-8 medium-9 small-12 details">
+                            <?php
+                            //do_action( 'woocommerce_after_single_product_summary' );
+                            $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
+
+                            if ( ! empty( $product_tabs ) ) : ?>
+
+                                <div class="woocommerce-tabs wc-tabs-wrapper">
+                                    <ul class="tabs wc-tabs" role="tablist">
+			                            <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+                                            <li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+                                                <a href="#tab-<?php echo esc_attr( $key ); ?>">
+						                            <?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
+                                                </a>
+                                            </li>
+			                            <?php endforeach; ?>
+                                    </ul>
+		                            <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+                                        <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+				                            <?php
+				                            if ( isset( $product_tab['callback'] ) ) {
+					                            call_user_func( $product_tab['callback'], $key, $product_tab );
+				                            }
+				                            ?>
+                                        </div>
+		                            <?php endforeach; ?>
+
+		                            <?php do_action( 'woocommerce_product_after_tabs' ); ?>
+                                </div>
+
+                            <?php endif; ?>
                         </div>
                     </div>
 
+                    <div class="kosnic-related">
+	                <?php
+	                echo do_shortcode('[related_products limit="12"]');
+                    ?>
+                    </div>
                     <?php
                     /**
                      * Hook: woocommerce_before_single_product_summary.
