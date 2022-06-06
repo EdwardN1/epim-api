20ea7138791a0a3cdc678f5da7b77e0776cef055
edwar <edward@technicks.com>
2022-06-06 09:59:27 +0100

    Dynamic Datasheets

--- wp-content/plugins/epim-api-importer/includes/autoinc-product-tabs.php
+++ wp-content/plugins/epim-api-importer/includes/autoinc-product-tabs.php
@@ -26,24 +26,58 @@ function epimaapi_product_tab($tabs)
 
 function epimaapi_product_tab_content()
 {
+    global $product;
+    $use_dynamic_data_sheets = get_option('epim_use_dynamic_data_sheets');
 
-    $datasheets = get_post_meta(get_the_ID(), '_epim_data_sheets', true);
-
-    if ($datasheets) {
-        if (is_array($datasheets)) {
+    if ($use_dynamic_data_sheets['checkbox_value'] == '1') {
+        $dynamic_data_sheets_url = get_option('epim_dynamic_data_sheets_url');
+        $dynamic_data_sheets_templates = preg_split('/\r\n|[\r\n]/', get_option('epim_dynamic_data_sheets_templates'));
+        $dynamic_data_sheets_names = preg_split('/\r\n|[\r\n]/', get_option('epim_dynamic_data_sheets_names'));
+        if ($dynamic_data_sheets_url && $dynamic_data_sheets_templates && $dynamic_data_sheets_names) {
             ?>
             <table class="woocommerce-product-attributes shop_attributes">
                 <?php
-                foreach ($datasheets as $datasheet) {
-                    ?>
-                    <tr>
-                        <td><a href="<?php echo $datasheet['URL']; ?>" target="_blank"><?php echo $datasheet['Name']; ?></a> </td>
-                    </tr>
-                    <?php
+                if (is_array($dynamic_data_sheets_templates)) {
+                    $i = 0;
+                    foreach ($dynamic_data_sheets_templates as $data_sheets_template) {
+                        $data_sheet_name = 'Data Sheet';
+                        if($dynamic_data_sheets_names[$i]) {
+                            $data_sheet_name = $dynamic_data_sheets_names[$i];
+                        }
+                        ?>
+                        <tr>
+                            <td><a href="<?php echo $dynamic_data_sheets_url.'?part='.$product->get_sku().'&template='.$data_sheets_template; ?>" target="_blank"><?php echo $data_sheet_name; ?></a> </td>
+                        </tr>
+                        <?php
+                    }
+                    $i++;
                 }
                 ?>
+
             </table>
             <?php
         }
+    } else {
+        $datasheets = get_post_meta(get_the_ID(), '_epim_data_sheets', true);
+
+        if ($datasheets) {
+            if (is_array($datasheets)) {
+                ?>
+                <table class="woocommerce-product-attributes shop_attributes">
+                    <?php
+                    foreach ($datasheets as $datasheet) {
+                        ?>
+                        <tr>
+                            <td><a href="<?php echo $datasheet['URL']; ?>"
+                                   target="_blank"><?php echo $datasheet['Name']; ?></a></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </table>
+                <?php
+            }
+        }
     }
+
 }
