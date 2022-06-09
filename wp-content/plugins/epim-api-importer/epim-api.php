4fd421c95827b40548d44a4f5fbfcfb121833609
EdwardN1 <GH?841241ed>
2022-06-09 14:23:40 +0100

    EPIM Tidy Up

--- wp-content/plugins/epim-api-importer/epim-api.php
+++ wp-content/plugins/epim-api-importer/epim-api.php
@@ -18,10 +18,31 @@ define('epimaapi_PLUGINPATH', plugin_dir_path( __FILE__ ) );
 define('epimaapi_PLUGINURI', plugin_dir_url(__FILE__));
 define('epimaapi_PLUGINFILE',__FILE__);
 $active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+
 if ( in_array( 'woocommerce/woocommerce.php', $active_plugins ) ) {
+	if( !in_array('click-collect/click-collect.php',$active_plugins)) {
+		define('cac_FUNCTIONSPATH', plugin_dir_path( __FILE__ ) . '/includes/');
+		define('cac_PLUGINPATH', plugin_dir_path( __FILE__ ) );
+		foreach (glob(epimaapi_FUNCTIONSPATH.'autoinc-candc-*.php') as $filename)
+		{
+			require_once ($filename);
+		}
+	}
+	if( !in_array('woo-purchase-on-account/woo-purchase-on-account.php',$active_plugins)) {
+		define('wpoa_FUNCTIONSPATH', plugin_dir_path( __FILE__ ) . '/includes/');
+		define('wpoa_PLUGINPATH', plugin_dir_path( __FILE__ ) );
+		foreach (glob(epimaapi_FUNCTIONSPATH.'autoinc-wpoa-*.php') as $filename)
+		{
+			require_once ($filename);
+		}
+	}
 	foreach (glob(epimaapi_FUNCTIONSPATH.'autoinc-*.php') as $filename)
 	{
-		require_once ($filename);
+		if(strpos($filename,'autoinc-wpoa')===false) {
+			if(strpos($filename,'autoinc-candc')===false) {
+				require_once ($filename);
+			}
+		}
 	}
 }
 
