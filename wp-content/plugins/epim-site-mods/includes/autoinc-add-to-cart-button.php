9595475ebd23cd42aea521287af669be808e3585
edwar <edward@technicks.com>
2022-07-07 15:57:52 +0100

    Update ePim to remove PictureIdsGrouped

--- wp-content/plugins/epim-site-mods/includes/autoinc-add-to-cart-button.php
+++ wp-content/plugins/epim-site-mods/includes/autoinc-add-to-cart-button.php
@@ -4,7 +4,22 @@ add_action('woocommerce_after_shop_loop_item', 'epsm_add_to_cart_button', 10);
 function epsm_add_to_cart_button()
 {
     ?>
-    <a href="<?php the_permalink(); ?>" class="more">More info</a>
+    <div class="add-to-basket-loop">
+        <?php
+        echo do_shortcode('[add_to_cart id="' . get_the_ID() . '" show_price="FALSE" style="border: none; padding:0;"]');
+        ?>
+    </div>
     <?php
-    echo do_shortcode('[add_to_cart id="'.get_the_ID().'" show_price="FALSE" style="border: none; padding:0;"]');
+}
+
+add_action('woocommerce_after_shop_loop_item_title', 'epsm_display_sku', 10);
+
+function epsm_display_sku()
+{
+    global $product;
+    ?>
+    <div class="product-sku">
+        SKU: <?php echo $product->get_sku();?>
+    </div>
+    <?php
 }
