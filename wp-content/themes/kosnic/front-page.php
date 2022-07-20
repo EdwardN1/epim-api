465c085ed115ef4808f7773e7560ca671647dac3
EdwardN1 <GH?841241ed>
2022-07-20 11:24:21 +0100

    Kosnic Homepage Content for Elementor

--- wp-content/themes/kosnic/front-page.php
+++ wp-content/themes/kosnic/front-page.php
@@ -6,6 +6,14 @@ if(have_posts()): while(have_posts()): the_post();
   $homepage_fields = new CMB2Fields(get_the_ID());
   $homepage_fields->field_prefix = 'homepage_';
   $homepage_fields->render('templates/hero-banner-tpl.php');
+	?>
+	<div id="the_content">
+		<?php
+		the_content();
+		?>
+	</div>
+<?php
 endwhile; endif;
+
 
 get_footer();
