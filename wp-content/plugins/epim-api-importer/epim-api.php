d7b580a61d32e559baa31d9fab7dbc07b5f8ddb9
edwar <edward@technicks.com>
2023-01-27 19:12:48 +0000

    SVN Update to Plugin Dir 1.0.8

--- wp-content/plugins/epim-api-importer/epim-api.php
+++ wp-content/plugins/epim-api-importer/epim-api.php
@@ -9,7 +9,7 @@ Plugin URI: https://e-pim.co.uk
 Description: This plugin requires you to have an account at https://epim.online and an activated epim api. You wil then be able to import your product data from multiple print and digital sources straight into WooCommerce for an instant online shop.
 Author: Edward Nickerson
 License: GPLv2 or later
-Version: 1.0.7
+Version: 1.0.8
 Author URI: https://www.technicks.com
 */
 
