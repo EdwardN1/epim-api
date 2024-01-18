44e539f1ca37412c834ea48c4d9449f0b458272e
edwar <edward@technicks.com>
2024-01-18 21:56:59 +0000

    Sylvania Setup

--- wp-content/plugins/Sylvania/includes/autoinc-api.php
+++ wp-content/plugins/Sylvania/includes/autoinc-api.php
@@ -1,13 +1,13 @@
 <?php
 
 function swp_make_api_call( $url ) {
-    $response = wp_safe_remote_get( $url . $url);
+    $response = wp_safe_remote_get( $url);
     if ( is_array( $response ) && ! is_wp_error( $response ) ) {
         $apiCall = $response['body'];
     } else {
         if ( is_wp_error( $response ) ) {
-            //error_log($response->get_error_message());
-            //error_log('URL called: '.$epim_url . $url);
+            error_log($response->get_error_message());
+            error_log('URL called: '. $url);
             $apiCall = $response->get_error_message();
         }
     }
